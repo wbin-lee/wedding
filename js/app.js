ebf8500184e442261e77173fa15047b159cebbc2
Woobin <leewb2002@gmail.com>
2026-07-27 01:35:52 +0900

    Use teaser_hero.jpg for Kakao Share feed image
    Kakao.Share.sendDefault was hardcoding hero.png, so button shares
    (일정 추가하기 / 청첩장 보러가기) ignored the updated OG image.

--- js/app.js
+++ js/app.js
@@ -864,7 +864,7 @@ function shareKakao() {
       content: {
         title: `${WEDDING.groom} ♡ ${WEDDING.bride}, 우리 결혼합니다 💍`,
         description: `${WEDDING.dateKo}\n${WEDDING.venue}`,
-        imageUrl: siteBase() + '/images/hero.png',
+        imageUrl: siteBase() + '/images/teaser_hero.jpg',
         link: { mobileWebUrl: invitationUrl, webUrl: invitationUrl },
       },
       buttons: [
